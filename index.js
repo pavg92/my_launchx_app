033a847d3d0a7a91d48d81b4e14d4168b27d3564
Pedro Angel <pavg_92@hotmail.com>
2022-05-17 22:04:00 -0500

    Creación de una clase MissionCommander  y modularización

---
+++ index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+console.log(carlo.name)
+const fer = new MissionCommander("Fer")
+console.log(fer.name)
+const rodrigo = new MissionCommander("Rodrigo")
+console.log(rodrigo.name)
